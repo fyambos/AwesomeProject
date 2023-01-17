a4272516640e5aecaaf0ad883c449eb8c8c0cf2b
fyambos <feliciteyambos@gmail.com>
2023-01-17 15:45:15 +0100

    chuck norris api read json data

--- src/screens/Chuck.js
+++ src/screens/Chuck.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 
 const Chuck = ({navigation}) => {
     const onPressHandler = () => {
         navigation.navigate('Home');
     }
+
+    const [fact, setFact] = useState(null)
+
+    getChuckFact = async () => {
+        const res = await fetch("https://api.chucknorris.io/jokes/random");
+        const chuckFact = await res.json();
+        console.log(res);
+        console.log(chuckFact);
+        setFact(chuckFact.value);
+    }
+
+    useEffect(()=> {
+        getChuckFact();
+    
+    }, []);
+
     return (
         <View>
             <Text>Home Page</Text>
             <TouchableOpacity onPress={onPressHandler}>
                 <Text>Go To Home Page</Text>
             </TouchableOpacity>
+            <Text>{fact}</Text>
         </View>
     )
 }
